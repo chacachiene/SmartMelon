3c4e0b13b7e8efc652245e9ef8078f7a73e2e1cf
Huy Ngo <www.huyngo@gmail.com>
2023-11-28 09:43:20 +0700

    merge code dev

--- client/src/pages/Visualize/ChartPage.jsx
+++ client/src/pages/Visualize/ChartPage.jsx
@@ -1,77 +1,73 @@
-import React, { useEffect } from "react"
-import { useState } from "react"
-import { BarChart } from "@mui/x-charts/BarChart"
-import { Helmet } from "react-helmet"
-import { Box, Button, Container, Grid, Paper, Stack, Typography, TextField } from "@mui/material"
-import { LineChart } from "@mui/x-charts"
-import styled from "@emotion/styled"
-import { Cached } from "@mui/icons-material"
-import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs"
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
-import { DatePicker } from "@mui/x-date-pickers"
-import dayjs from "dayjs"
+import React, { useEffect } from "react";
+import { useState } from "react";
+import { BarChart } from "@mui/x-charts/BarChart";
+import { Helmet } from "react-helmet";
+import {
+  Box,
+  Button,
+  Container,
+  Grid,
+  Paper,
+  Stack,
+  Typography,
+  TextField,
+} from "@mui/material";
+import { LineChart } from "@mui/x-charts";
+import styled from "@emotion/styled";
+import { Cached } from "@mui/icons-material";
+import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
+import { DatePicker } from "@mui/x-date-pickers";
+import dayjs from "dayjs";
 
 const ChartPage = ({ Namepage, data }) => {
-  const [dataSeries, setDataSeries] = useState(Array.from({ length: 24 }, (_, index) => null))
-  const [dataSeries1, setDataSeries1] = useState(Array.from({ length: 24 }, (_, index) => null))
-  const dataAxis = Array.from({ length: 24 }, (_, index) => index)
-  const [selectedDate, setSelectedDate] = useState(dayjs("2023-10-29T15:07:15Z"))
+  const [dataSeries, setDataSeries] = useState(
+    Array.from({ length: 24 }, (_, index) => null)
+  );
+  const [dataSeries1, setDataSeries1] = useState(
+    Array.from({ length: 24 }, (_, index) => null)
+  );
+  const dataAxis = Array.from({ length: 24 }, (_, index) => index);
+  const [selectedDate, setSelectedDate] = useState(
+    dayjs("2023-10-29T15:07:15Z")
+  );
 
-  // useEffect(() => {
-  //   if (selectedDate) {
-  //     const selectedDay = dayjs(selectedDate).format("DD/MM/YYYY")
-
-  //     setDataSeries1(Array.from({ length: 24 }, (_, index) => null))
-  //     data.forEach((item) => {
-  //       const date = dayjs(item.created_at).format("DD/MM/YYYY")
-  //       if (date === selectedDay) {
-  //         const hour = dayjs(item.created_at).format("H")
-  //         dataSeries1[hour - 7] = Number(item.value)
-  //       }
-  //     })
-  //   }
-  // }, [selectedDate])
   useEffect(() => {
-    
-
     if (selectedDate) {
-      const selectedDay = dayjs(selectedDate).format("DD/MM/YYYY")
+      const selectedDay = dayjs(selectedDate).format("DD/MM/YYYY");
 
-      setDataSeries1(Array.from({ length: 24 }, (_, index) => null))
+      setDataSeries1(Array.from({ length: 24 }, (_, index) => null));
       data.forEach((item) => {
-        const date = dayjs(item.created_at).format("DD/MM/YYYY")
+        const date = dayjs(item.created_at).format("DD/MM/YYYY");
         if (date === selectedDay) {
-          const hour = dayjs(item.created_at).format("H")
-          dataSeries1[hour - 7] = Number(item.value)
+          const hour = dayjs(item.created_at).format("H");
+          dataSeries1[hour - 7] = Number(item.value);
         }
-      })
+      });
 
-      // Gán dataSeries1 vào một mảng tạm thời
-      const tempDataSeries = [...dataSeries1]
-      setDataSeries(tempDataSeries)
+      const tempDataSeries = [...dataSeries1];
+      setDataSeries(tempDataSeries);
     }
-  }, [selectedDate])
+  }, [selectedDate]);
 
   // ...
 
   const handleDateChange = (date) => {
-    setSelectedDate(date)
-  }
-  // useEffect(() => {
-  //   setDataSeries(dataSeries1)
-  // }, [dataSeries1])
-  // useEffect(() => {
-  //   console.log("dataseries12312312", dataSeries)
-  // }, [dataSeries])
+    setSelectedDate(date);
+  };
+
   return (
     <>
       <Container>
-        <Typography variant="h3" sx={{ marginBottom: "10px", marginTop: "10px" }}>
+        <Typography
+          variant="h3"
+          sx={{ marginBottom: "10px", marginTop: "10px" }}
+        >
           Energy Management/
           {Namepage}
         </Typography>
         <Paper>
-          <Grid container>
+          <Grid container style={{ padding: "10px" }}>
             <Grid item xs={10}>
               <LineChart
                 xAxis={[
@@ -92,16 +88,31 @@ const ChartPage = ({ Namepage, data }) => {
                 <Grid item xs={1}></Grid>
                 <Grid item xs={3}>
                   <LocalizationProvider dateAdapter={AdapterDayjs}>
-                    <DatePicker value={selectedDate} onChange={handleDateChange} />
+                    <DatePicker
+                      value={selectedDate}
+                      onChange={handleDateChange}
+                    />
                   </LocalizationProvider>
                 </Grid>
                 <Grid item xs={4}>
-                  <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
                     <h3>Status: Normal</h3>
                   </div>
                 </Grid>
                 <Grid item xs={4}>
-                  <div style={{ display: "flex", justifyContent: "center", alignItems: "center" }}>
+                  <div
+                    style={{
+                      display: "flex",
+                      justifyContent: "center",
+                      alignItems: "center",
+                    }}
+                  >
                     <h3>Current: 80%</h3>
                     <Button>
                       <Cached fontSize="large" />
@@ -138,24 +149,24 @@ const ChartPage = ({ Namepage, data }) => {
         </Paper>
       </Container>
     </>
-  )
-}
+  );
+};
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
   ...theme.typography.body2,
   padding: theme.spacing(1),
   textAlign: "center",
   color: theme.palette.text.secondary,
-}))
+}));
 const CustomButton = styled(Button)`
   width: 150px;
   height: 50px;
-`
+`;
 const ButtonContainer = styled(Box)`
   width: 100%;
   height: 80px;
   display: flex;
   justify-content: center;
   align-items: center;
-`
-export default ChartPage
+`;
+export default ChartPage;
